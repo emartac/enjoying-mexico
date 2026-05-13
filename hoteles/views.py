05265767bf106214378ad80025da5eabd2797e84
Edgar <ed.910520.em@gmail.com>
2026-05-13 17:54:48 -0600

    Group habitaciones by viaje in list view

--- hoteles/views.py
+++ hoteles/views.py
@@ -69,8 +69,32 @@ class HabitacionListView(LoginRequiredMixin, ListView):
         return qs
 
     def get_context_data(self, **kwargs):
+        from collections import OrderedDict
+        from viajes.models import ViajeHabitacion
         ctx = super().get_context_data(**kwargs)
-        ctx['q'] = self.request.GET.get('q', '')
+        q = self.request.GET.get('q', '')
+        ctx['q'] = q
+
+        vh_qs = (
+            ViajeHabitacion.objects
+            .select_related('viaje', 'habitacion__tipo')
+            .order_by('viaje__fecha_salida', 'viaje__nombre', 'habitacion__nombre_hotel', 'habitacion__numero')
+        )
+        if q:
+            vh_qs = vh_qs.filter(habitacion__nombre_hotel__icontains=q) | vh_qs.filter(habitacion__numero__icontains=q)
+
+        grupos = OrderedDict()
+        asignadas_ids = set()
+        for vh in vh_qs:
+            vid = vh.viaje_id
+            if vid not in grupos:
+                grupos[vid] = {'viaje': vh.viaje, 'habitaciones': []}
+            grupos[vid]['habitaciones'].append(vh.habitacion)
+            asignadas_ids.add(vh.habitacion_id)
+
+        sin_viaje = [h for h in ctx['habitaciones'] if h.pk not in asignadas_ids]
+        ctx['grupos'] = grupos.values()
+        ctx['sin_viaje'] = sin_viaje
         return ctx
 
 
